e7a5e1b059a597946258ae0ebc03daa788737180
Jeremy MARMOL <jeremy.marmol@fr.clara.net>
2022-09-30 12:04:33 +0200

    AZ-840: apply suggestions

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.38"
+      version = "~> 3.0"
     }
     null = {
       source  = "hashicorp/null"
