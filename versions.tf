109e7b4c8ef4e2a3e59e24364c8e2c1908565320
Benjamin DUPUIS <benjamin.dupuis@fr.clara.net>
2024-07-19 11:45:24 +0200

    feat: add semantic_search_sku

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.72"
+      version = "~> 3.78"
     }
     null = {
       source  = "hashicorp/null"
